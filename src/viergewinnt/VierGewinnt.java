c7e379d55889b88d7020462df54a5c01e0badc2b
Hakulukiam <markus.ruckenbauer@kinamu.com>
2017-06-16 01:29:28 +0200

    img + Base Layout

--- src/viergewinnt/VierGewinnt.java
+++ src/viergewinnt/VierGewinnt.java
@@ -8,16 +8,70 @@ package viergewinnt;
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 public class VierGewinnt extends Applet implements WindowListener {
-
-   public static void main(java.lang.String[] args) {
+    
+   private static Label upperLabel;
+   private static Panel buttonPanel;   
+   private static Frame w;
+   private static Font font;
+    
+   public static void main(java.lang.String[] args) {      
       VierGewinnt applet = new VierGewinnt();
-      Frame w = new Frame("Vier Gewinnt");
-      w.addWindowListener(applet);
-      w.add("Center", applet);
-      w.setSize(150, 50);
+      font = new Font ("Sans Serif", 1, 25);
+      w = new Frame("Vier Gewinnt");
+      w.addWindowListener(applet);         
+      w.setSize(940, 629);
+      w.setResizable(false);      
+      
+      upperLabel = new Label();        
+      upperLabel.setAlignment(Label.CENTER);
+      upperLabel.setSize(250,100);
+      upperLabel.setLocation(702, 0);
+      upperLabel.setBackground(Color.GRAY);
+      upperLabel.setForeground(Color.WHITE);
+      upperLabel.setFont(font);
+      upperLabel.setText("Vier Gewinnt!");
+      
+      Button newGameButton = new Button("Neues Spiel");
+      Button closeButton = new Button("Beenden");
+      
+
+      newGameButton.addActionListener((ActionEvent e) -> {
+          upperLabel.setText("Neues Spiel");
+      });
+
+      closeButton.addActionListener((ActionEvent e) -> {
+          w.dispose();
+      });
+      
+      buttonPanel = new Panel();
+      buttonPanel.setLayout(new GridBagLayout());
+      GridBagConstraints gbc = new GridBagConstraints();
+      gbc.fill = GridBagConstraints.HORIZONTAL;
+      gbc.gridwidth = 3;
+      gbc.ipadx = 80;
+      gbc.ipady = 15;
+      gbc.gridx = 0;
+      gbc.gridy = 0;
+      buttonPanel.add(newGameButton,gbc);
+      gbc.insets = new Insets(10,0,0,0);
+      gbc.gridy = 1;
+      buttonPanel.add(closeButton,gbc);      
+      buttonPanel.setSize(240, 200);
+      buttonPanel.setLocation(702, 400);
+      buttonPanel.setBackground(Color.GRAY);
+      
+      w.add(buttonPanel);
+      w.add(upperLabel);
+      w.add(applet);
       w.setVisible(true);
+      
       applet.init();
       applet.start();
    }
@@ -25,7 +79,15 @@ public class VierGewinnt extends Applet implements WindowListener {
    @Override
    public void paint(Graphics g) {
       super.paint(g);
-      g.drawString("Hello World", 10, 10);	// Upper left corner
+      Image image;
+       try {
+           image = ImageIO.read(new File("vierGewinntBoardSmall.png"));
+           g.drawImage(image, 0, 0, null);
+       } catch (IOException ex) {
+           Logger.getLogger(VierGewinnt.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       g.setColor(Color.gray);
+       g.fillRect(699, 0, 250, 600);       
    }
 
    @Override
