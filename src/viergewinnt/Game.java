eeab0fbba8cd4f2ebba8052a6f794c55aae17f86
Nuoxx <King_Woozie@web.de>
2017-06-17 15:37:44 +0200

    22

--- src/viergewinnt/Game.java
+++ src/viergewinnt/Game.java
@@ -145,6 +145,21 @@ public class Game {
     }
     
     private Boolean checkDiagonals(Integer x, Integer y){
+        int d = 0;
+        for(int i = 0; i < 5; i++) {
+            if(x-y <= 0) {
+                x = 0;
+                y = (x-y) * (-1);               
+                if(field[x+i][y+i].equals(currentTurn)) {
+                    d++;
+                }else{
+                    d = 0;
+                }          
+            }  
+            if(d >= 4){
+                return true;
+            }                                                                            
+        }   
         return false;
     }
         
