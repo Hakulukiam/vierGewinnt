eff1dc0c487ae9d3e4ce26ec9602db78d1997b31
Hakulukiam <markus.ruckenbauer@kinamu.com>
2017-06-17 10:43:32 +0200

    7

--- src/viergewinnt/Game.java
+++ src/viergewinnt/Game.java
@@ -5,6 +5,8 @@
  */
 package viergewinnt;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Marcel
@@ -13,5 +15,39 @@ public class Game {
     
     private Player playerOne;
     private Player playerTwo;
+    private Player currentTurn;
+    private GUI gui;
+    private Integer[][] field;
+    private static final Integer[] SIZE = {7,6};
+
+    public Game(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.field = new Integer[SIZE[0]][SIZE[1]];
+        Arrays.fill(this.field, 0);
+        this.gui = new GUI();
+    }
+    
+    public Player getPlayerOne() {
+        return playerOne;
+    }
+
+    public void setPlayerOne(Player playerOne) {
+        this.playerOne = playerOne;
+    }
+
+    public Player getPlayerTwo() {
+        return playerTwo;
+    }
+
+    public void setPlayerTwo(Player playerTwo) {
+        this.playerTwo = playerTwo;
+    }
+    
+    public Boolean isValidMove(){
+        return true;
+    }
+    
+    
     
 }
