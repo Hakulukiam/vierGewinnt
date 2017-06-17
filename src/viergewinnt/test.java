9d577da18565edd0d3398ad436e05f18bd80ffe2
Hakulukiam <markus.ruckenbauer@kinamu.com>
2017-06-17 15:52:13 +0200

    24

--- src/viergewinnt/test.java
+++ src/viergewinnt/test.java
@@ -10,18 +10,25 @@
  * @author Marcel
  */
 /*public class test {
- private Boolean checkRows(Integer x, Integer y){
-        
-           int d = 0;
-           for(int i = 0; i < 8; i++) {
-           if(field[i][y] == currentTurn) {
-              d++;
-           }else{
-              d = 0;
-           }  
-           if(d >= 4){
-               return true;
+    private Boolean checkDiagonals(Integer x, Integer y){
+        int d = 0;
+        for(int i = 0; i < 5; i++) {
+            if(x-y <= 0) {
+                x = 0;
+                y = (x-y) * (-1); 
+                if (x+i < 0 || x+i >= SIZE[0] || y+i < 0 || y+i >= SIZE[1]) {
+                    break;
+                }else{
+                    if(field[x+i][y+i].equals(currentTurn)) {
+                        d++;
+                    }else{
+                        d = 0;
+                    }
+                }           
+            }  
+            if(d >= 4){
+                return true;
             }                                                                            
         }   
-           return false;
+        return false;
     }*/
